8d0972436613f862b969eff4aa1f16c7b3964f52
tsuchiya-yu <nomoreheronomorehero@gmail.com>
2024-04-29 13:58:51 +0900

    add group関連の画面を追加

---
+++ app/controllers/mypage/groups_controller.rb
@@ -0,0 +1,49 @@
+class Mypage::GroupsController < Mypage::MypageApplicationController
+
+    def new
+        @group = Group.new
+    end
+
+    def create
+        group = Group.new(group_params)
+        if group.save && group.group_members.create(user: current_user, is_master: true)
+          redirect_to mypage_path, notice: 'グループを作成しました'
+        else
+          render :new, status: :unprocessable_entity
+        end
+    end
+
+    def show
+      @group = current_user.groups.find(params[:id])
+    end
+
+    def edit
+      @group = current_user.groups.find(params[:id])
+    end
+
+    def update
+      @group = current_user.groups.find(params[:id])
+      if @group.update(group_params)
+        redirect_to mypage_group_path(@group), notice: '更新しました'
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def confirm_destroy
+      @group = current_user.groups.find(params[:id])
+    end
+
+    def destroy
+      @group = current_user.groups.find(params[:id])
+      @group.destroy!
+      redirect_to mypage_path, notice: 'グループを削除しました'
+    end
+
+    private
+
+    def group_params
+        params.require(:group).permit(:name, :catchphrase)
+    end
+
+end
